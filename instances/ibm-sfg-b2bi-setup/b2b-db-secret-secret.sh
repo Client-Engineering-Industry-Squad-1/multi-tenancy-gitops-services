e8dcbf5ac70c19b9a82fce98b965d1d8a74df84e
Steve Cotugno <steve.cotugno@us.ibm.com>
2022-05-16 06:03:53 -0700

    added db user parameter

--- instances/ibm-sfg-b2bi-setup/b2b-db-secret-secret.sh
+++ instances/ibm-sfg-b2bi-setup/b2b-db-secret-secret.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 # Set variables
+if [[ -z ${B2B_DB_USER} ]]; then
+  echo "Please provide environment variable B2B_DB_USER"
+  exit 1
+fi
+
 if [[ -z ${B2B_DB_SECRET} ]]; then
   echo "Please provide environment variable B2B_DB_SECRET"
   exit 1
 fi
 
+B2B_DB_USER=${B2B_DB_USER}
 B2B_DB_SECRET=${B2B_DB_SECRET}
 
 SEALED_SECRET_NAMESPACE=${SEALED_SECRET_NAMESPACE:-sealed-secrets}
@@ -13,7 +19,7 @@ SEALED_SECRET_CONTOLLER_NAME=${SEALED_SECRET_CONTOLLER_NAME:-sealed-secrets}
 
 # Create Kubernetes Secret yaml
 oc create secret generic b2b-db-secret --type=Opaque \
---from-literal=DB_USER=db2inst1 \
+--from-literal=DB_USER=${B2B_DB_USER} \
 --from-literal=DB_PASSWORD=${B2B_DB_SECRET} \
 --dry-run=client -o yaml > delete-b2b-db-secret-secret.yaml
 
